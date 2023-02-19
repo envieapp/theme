af90dfd0b0f05e010a8bfdb0a0d01699e8f1f8d3
BruhaBruh <drugsho.jaker@gmail.com>
2023-02-19 16:15:15 +0500

    feat: add opacity fn

--- src/context/theme/wrap-base-theme/index.ts
+++ src/context/theme/wrap-base-theme/index.ts
@@ -8,75 +8,66 @@ export const wrapBaseTheme = (baseTheme: BaseEnvieTheme, schemeName: SchemeName)
   envieTheme.sys.color = schemeFromRef(schemeName, baseTheme.ref);
   envieTheme.fn = {
     surface(level, color = envieTheme.sys.color.shadow) {
-      const r = color.argb >> 16 & 255;
-      const g = color.argb >> 8 & 255;
-      const b = color.argb & 255;
       const surface = envieTheme.sys.color.surface.hex;
       if (level === 1) {
-        return `linear-gradient(0deg, rgba(${r},${g},${b},0.05), `
-          + `rgba(${r},${g},${b},0.05)), ${surface}`;
+        const c = this.opacity(color, 0.05);
+        return `linear-gradient(0deg, ${c}, ${c}), ${surface}`;
       }
       if (level === 2) {
-        return `linear-gradient(0deg, rgba(${r},${g},${b},0.08), `
-          + `rgba(${r},${g},${b},0.08)), ${surface}`;
+        const c = this.opacity(color, 0.08);
+        return `linear-gradient(0deg, ${c}, ${c}), ${surface}`;
       }
       if (level === 3) {
-        return `linear-gradient(0deg, rgba(${r},${g},${b},0.11), `
-          + `rgba(${r},${g},${b},0.11)), ${surface}`;
+        const c = this.opacity(color, 0.11);
+        return `linear-gradient(0deg, ${c}, ${c}), ${surface}`;
       }
       if (level === 4) {
-        return `linear-gradient(0deg, rgba(${r},${g},${b},0.12), `
-          + `rgba(${r},${g},${b},0.12)), ${surface}`;
+        const c = this.opacity(color, 0.12);
+        return `linear-gradient(0deg, ${c}, ${c}), ${surface}`;
       }
       if (level === 5) {
-        return `linear-gradient(0deg, rgba(${r},${g},${b},0.14), `
-          + `rgba(${r},${g},${b},0.14)), ${surface}`;
+        const c = this.opacity(color, 0.14);
+        return `linear-gradient(0deg, ${c}, ${c}), ${surface}`;
       }
       return 'Invalid surface level';
     },
     elevation(level, color = envieTheme.sys.color.shadow) {
-      const r = color.argb >> 16 & 255;
-      const g = color.argb >> 8 & 255;
-      const b = color.argb & 255;
-      const shadowR = envieTheme.sys.color.shadow.argb >> 16 & 255;
-      const shadowG = envieTheme.sys.color.shadow.argb >> 8 & 255;
-      const shadowB = envieTheme.sys.color.shadow.argb & 255;
+      const crgba = this.opacity(color, 0.15);
+      const srgba = this.opacity(envieTheme.sys.color.shadow, 0.3);
       if (level === 1) {
-        return `0 1px 3px 1px rgba(${r},${g},${b},0.15), `
-            + `0 1px 2px rgba(${shadowR},${shadowG},${shadowB},0.3)`;
+        return `0 1px 3px 1px ${crgba}, 0 1px 2px ${srgba}`;
       }
       if (level === 2) {
-        return `0 2px 6px 2px rgba(${r},${g},${b},0.15), `
-            + `0 1px 2px rgba(${shadowR},${shadowG},${shadowB},0.3)`;
+        return `0 2px 6px 2px ${crgba}, 0 1px 2px ${srgba}`;
       }
       if (level === 3) {
-        return `0 4px 8px 3px rgba(${r},${g},${b},0.15), `
-            + `0 1px 3px rgba(${shadowR},${shadowG},${shadowB},0.3)`;
+        return `0 4px 8px 3px ${crgba}, 0 1px 3px ${srgba}`;
       }
       if (level === 4) {
-        return `0 6px 10px 4px rgba(${r},${g},${b},0.15), `
-            + `0 2px 3px rgba(${shadowR},${shadowG},${shadowB},0.3)`;
+        return `0 6px 10px 4px ${crgba}, 0 2px 3px ${srgba}`;
       }
       if (level === 5) {
-        return `0 8px 12px 6px rgba(${r},${g},${b},0.15), `
-            + `0 4px 4px rgba(${shadowR},${shadowG},${shadowB},0.3)`;
+        return `0 8px 12px 6px ${crgba}, 0 4px 4px ${srgba}`;
       }
       return 'Invalid elevation level';
     },
     stateLayer(level, color = envieTheme.sys.color.primary) {
+      if (level === 'hovered') {
+        return this.opacity(color, 0.08);
+      }
+      if (level === 'focused') {
+        return this.opacity(color, 0.12);
+      }
+      if (level === 'pressed') {
+        return this.opacity(color, 0.16);
+      }
+      return 'Invalid state layer level';
+    },
+    opacity(color, opacity) {
       const r = color.argb >> 16 & 255;
       const g = color.argb >> 8 & 255;
       const b = color.argb & 255;
-      if (level === 'hovered') {
-        return `rgba(${r},${g},${b},0.08)`;
-      }
-      if (level === 'focused') {
-        return `rgba(${r},${g},${b},0.12)`;
-      }
-      if (level === 'pressed') {
-        return `rgba(${r},${g},${b},0.16)`;
-      }
-      return 'Invalid state layer level';
+      return `rgba(${r},${g},${b},${opacity})`;
     },
   };
 
--- src/types/envie-theme-fn/index.ts
+++ src/types/envie-theme-fn/index.ts
@@ -4,4 +4,5 @@ export type EnvieThemeFn = {
   elevation: (level: 1 | 2 | 3 | 4 | 5, color?: ColorObject) => string
   surface: (level: 1 | 2 | 3 | 4 | 5, color?: ColorObject) => string
   stateLayer: (level: 'hovered' | 'focused' | 'pressed', color?: ColorObject) => string
+  opacity: (color: ColorObject, opacity: number) => string
 };
